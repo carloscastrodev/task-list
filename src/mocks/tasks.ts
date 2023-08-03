f33fa89f0ff51db039af476077ee2066108624c1
Carlos Castro <carlos_2009_vinicius@hotmail.com>
2023-08-03 19:48:16 -0300

    feat: add task types and mocks

---
+++ src/mocks/tasks.ts
@@ -0,0 +1,25 @@
+import { Task, TaskList, TaskStatus } from '@/types/task';
+
+export const taskMock: Task = {
+  id: 0,
+  description: 'Task',
+  status: TaskStatus.PENDING,
+  priority: 0,
+  createdAt: '2023-08-03 15:00:00',
+  subTasks: [],
+  parentTaskId: null,
+};
+
+const TASK_LIST_MOCK_LENGTH = 10;
+
+export const taskListMock: TaskList = Array.from({
+  length: TASK_LIST_MOCK_LENGTH,
+}).map((_, i) => ({
+  id: TASK_LIST_MOCK_LENGTH - i,
+  description: `Task ${i}`,
+  priority: TASK_LIST_MOCK_LENGTH - i,
+  status: TaskStatus.PENDING,
+  createdAt: '2023-08-03 15:00:00',
+  subTasks: [],
+  parentTaskId: null,
+}));
---
+++ src/types/task.ts
@@ -0,0 +1,22 @@
+// We're not using the shorthand assignment here because if someone changes the order of the enum keys,
+// it would also change the value assigned to each key. This way we're explicit as which value is which,
+// and it should not be changed.
+export enum TaskStatus {
+  DELETED = 0,
+  DONE = 1,
+  PENDING = 2,
+}
+
+export interface Task {
+  readonly id: number;
+  description: string;
+  status: TaskStatus;
+  priority: number;
+  parentTaskId: number | null;
+  subTasks: Task[];
+  readonly createdAt: string;
+  doneAt?: string;
+  deletedAt?: string;
+}
+
+export type TaskList = Task[];
