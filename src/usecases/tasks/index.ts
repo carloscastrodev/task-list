a6b1116c16f95a204cf0ae6e3fc268d36e5dc393
Carlos Castro <carlos_2009_vinicius@hotmail.com>
2023-08-04 17:56:06 -0300

    feat: add index exports of tasks usecases

---
+++ src/usecases/tasks/index.ts
@@ -0,0 +1,6 @@
+export { useTaskList } from './useTaskList';
+export { useCompleteTask } from './useCompleteTask';
+export { useCreateTask } from './useCreateTask';
+export { useDeleteTask } from './useDeleteTask';
+export { useReorderTasks } from './useReorderTasks';
+export { useCreateSubtask } from './useCreateSubtask';
