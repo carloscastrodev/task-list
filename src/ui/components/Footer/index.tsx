38e994c97cbe8e4b085bee517ee0c7938a297f55
Carlos Castro <carlos_2009_vinicius@hotmail.com>
2023-08-04 17:41:43 -0300

    feat: add footer component

---
+++ src/ui/components/Footer/index.tsx
@@ -0,0 +1,21 @@
+export function Footer() {
+  return (
+    <footer
+      className={`mt-auto h-[${footerHeightInPixels}px] border-t-2 border-slate-100`}
+    >
+      <p className="text-center text-sm">
+        Made by{' '}
+        <a
+          href="https://github.com/carloscastrodev"
+          rel="noopener noreferrer"
+          target="_blank"
+          className="text-bold underline underline-offset-4 hover:opacity-70"
+        >
+          @carloscastrodev
+        </a>
+      </p>
+    </footer>
+  );
+}
+
+export const footerHeightInPixels = 40;
