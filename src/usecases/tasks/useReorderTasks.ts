95941b8a1755f8d301c881d8f711d1991785b2de
Carlos Castro <carlos_2009_vinicius@hotmail.com>
2023-08-04 17:51:29 -0300

    feat(usecases): add usecase for reordering tasks

---
+++ src/usecases/tasks/useReorderTasks.ts
@@ -0,0 +1,77 @@
+import { MutateHook } from '@/types/hooks';
+import { TaskList } from '@/types/task';
+import { updateTasksPriority } from '@/utils';
+import { useMutation, useQueryClient } from '@tanstack/react-query';
+
+interface ReorderTaskData {
+  taskIndex: number;
+  destinationIndex: number;
+}
+
+export const useReorderTasks: MutateHook<ReorderTaskData> = (queryKey) => {
+  const queryClient = useQueryClient();
+
+  const taskList: TaskList = queryClient.getQueryData(queryKey) ?? [];
+
+  const reorderAndUpdatePriorities = ({
+    taskIndex,
+    destinationIndex,
+  }: {
+    taskIndex: number;
+    destinationIndex: number;
+  }) => {
+    const [removed] = taskList.splice(taskIndex, 1);
+    taskList.splice(destinationIndex, 0, removed);
+
+    updateTasksPriority(taskList);
+  };
+
+  const setQueryData = () => {
+    queryClient.setQueryData(queryKey, [...taskList]);
+  };
+
+  const { mutate, isLoading, isError } = useMutation<
+    Record<string, number>,
+    unknown,
+    ReorderTaskData,
+    void
+  >(
+    async ({ taskIndex, destinationIndex }) => {
+      reorderAndUpdatePriorities({ taskIndex, destinationIndex });
+
+      setQueryData();
+
+      const taskPriorityMap = taskList.reduce(
+        (acc, next) => {
+          acc[next.id] = next.priority;
+          return acc;
+        },
+        {} as Record<string, number>
+      );
+
+      //const task = actuallySendTaskToBackend()
+
+      return taskPriorityMap;
+    },
+    {
+      onError: (_, { taskIndex, destinationIndex }) => {
+        // report error to user
+        reorderAndUpdatePriorities({
+          taskIndex: destinationIndex,
+          destinationIndex: taskIndex,
+        });
+        setQueryData();
+      },
+    }
+  );
+
+  return {
+    mutate,
+    isLoading,
+    isError,
+    errorMessage:
+      isError && !isLoading
+        ? "Couldn't delete task. Reload the page and try again."
+        : undefined,
+  };
+};
