1d5f911cb98b4dda618b4456d6cee5fd271d5ed7
Carlos Castro <carlos_2009_vinicius@hotmail.com>
2023-08-04 18:06:25 -0300

    feat: add main john's task list page to app

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,20 @@
 import { ReactQueryProvider } from '@/packages/@tanstack/react-query/ReactQueryProvider';
+import { Footer, footerHeightInPixels } from '@/ui/components/Footer';
+import { Layout } from '@/ui/components/Layout';
+import JohnsTaskList from '@/ui/pages/JohnsTaskList';
 
 function App() {
-  return <ReactQueryProvider></ReactQueryProvider>;
+  return (
+    <ReactQueryProvider>
+      <Layout>
+        <main className={`h-[calc(100vh-${footerHeightInPixels}px)]`}>
+          <JohnsTaskList />
+        </main>
+
+        <Footer />
+      </Layout>
+    </ReactQueryProvider>
+  );
 }
 
 export default App;
