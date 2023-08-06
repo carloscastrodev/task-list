3a8d3843b89b03bf166d735f0afa65b01fa61bec
Carlos Castro <carlos_2009_vinicius@hotmail.com>
2023-08-06 15:15:26 -0300

    fix: api address

--- src/services/tasks/completeTask.ts
+++ src/services/tasks/completeTask.ts
@@ -2,7 +2,7 @@ import api from '@/services/api';
 import { Task } from '@/types/task';
 
 export async function completeTask(id: number) {
-  const { data } = await api.put<Task>(`/tasks/completeTask/${id}`);
+  const { data } = await api.put<Task>(`/tasks/complete-task/${id}`);
 
   return data;
 }
